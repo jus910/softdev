76f624642135f417ccc31f1534724b4eae8337a7
Jason Mohabir <jmohabir30@stuy.edu>
2022-09-21 21:28:25 -0400

    list2

--- 03_py/warmup2/warmup2.py
+++ 03_py/warmup2/warmup2.py
@@ -1,3 +1,25 @@
+
+def string_times(str, n):
+  return str*n
+
+print(string_times('Hi', 2))
+
+def string_bits(str):
+  new_str = ''
+  for i in range(0,len(str),2):
+    new_str += str[i:i+1]
+  return new_str
+
+print(string_bits('Hello'))
+
+def string_splosion(str):
+  new_str = ''
+  for i in range(len(str)):
+    new_str+=str[:i+1]
+  return new_str
+
+print(string_splosion('jerry'))
+
 def front_times(str, n):
   return str[:3] * n
 print(front_times('Chocolate', 2))
