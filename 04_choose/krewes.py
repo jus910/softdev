51657b3fd39f87a007490b3537957b0ae2f4c2c8
Justin <jmohabir30@stuy.edu>
2022-09-22 14:08:57 -0400

    go

--- 04_choose/krewes.py
+++ 04_choose/krewes.py
@@ -9,11 +9,20 @@ time spent:
 DISCO:
   - Key must be defined
   - list is an unhashable type
+  - randint(a,b) returns an integer from [a,b]
 QCC:
 OPS SUMMARY: how program operates
 """
+import random as rng
 
-krewes = {'bruh':{'bruh':'bruhbruh'},1:['b'],2:['c']}
+krewes = {2:['bruh','1'],7:['b'],8:['c']}
+
+def randomPerson():
+    peeps=[]
+    for n in krewes.values():
+        peeps+=n
+#     return rng.choice(peeps)
+    return peeps
 for i in range(100):
-    print(random)
-print(krewes['bruh'])
+    print(randomPerson())
+print(krewes.values())
