ee3828b3068734e8e2501153ef305f55e8fbf304
Justin <jmohabir30@stuy.edu>
2022-09-22 13:54:45 -0400

    krewes

---
+++ 04_choose/krewes.py
@@ -0,0 +1,19 @@
+"""
+Humans: Maya Mori, Justin Mohabir, Gordan Mo
+Duckies: Catbat, Alfred, Blueman
+
+Softdev
+04_choose
+2022-09-23
+time spent:
+DISCO:
+  - Key must be defined
+  - list is an unhashable type
+QCC:
+OPS SUMMARY: how program operates
+"""
+
+krewes = {'bruh':{'bruh':'bruhbruh'},1:['b'],2:['c']}
+for i in range(100):
+    print(random)
+print(krewes['bruh'])
